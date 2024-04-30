0aa37958a211d50b36624ee22c6af9a371049944
Nfjdbfhr <114594474+Nfjdbfhr@users.noreply.github.com>
2024-04-30 10:42:54 -0400

    Update InventoryManager.cs

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -96,6 +96,31 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
+    public void unEquip(int index)
+    {
+        if (inventory["equipped"][index] == null)
+        {
+            return;
+        }
+        if (index == 0)
+        {
+            inventory["weapons"].Add(inventory["equipped"][0]);
+        }
+        if (index == 1)
+        {
+            inventory["shields"].Add(inventory["equipped"][1]);
+        }
+
+        if (GameObject.Find(inventory["equipped"][index]) != null)
+        {
+            GameObject.Find(inventory["equipped"][index]).transform.localScale = Vector3.zero;
+        }
+
+        inventory["equipped"][index] = null;
+
+        inventoryDisplay.loadTab();
+    }
+
     public string itemTypeToEquip;
 
     public void setEquipItemType(string itemType)
@@ -145,5 +170,16 @@ public class InventoryManager : MonoBehaviour
         inventory["equipped"][indexToAddTo] = item; // Add the item to the "equipped" list
 
         GameObject.Find(inventory["equipped"][indexToAddTo]).transform.localScale = GameObject.Find(inventory["equipped"][indexToAddTo]).GetComponent<Item>().originalSize;
+
+        if (itemTypeToEquip == "weapons")
+        {
+            inventory["weapons"].RemoveAt(index);
+        }
+        if (itemTypeToEquip == "shields")
+        {
+            inventory["shields"].RemoveAt(index);
+        }
+
+        inventoryDisplay.loadTab();
     }
 }
